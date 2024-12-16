b3ff7743c6ce72658b31b2ddf7805e4be6713f74
Akshay S <adiosphobian@gmail.com>
2024-12-17 01:04:39 +0530

    day4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -43,6 +43,20 @@ enum Dir{
     RightD,
 }
 impl InnerState{
+    fn check_diag(&self) -> bool{
+        if self[4] != 2{
+            return false
+        }
+        match (self[0],self[8],self[2], self[6]){
+            (1,3,1,3) => true ,
+            (3,1,3,1) => true ,
+            (3,1,1,3) => true ,
+            (1,3,3,1) => true ,
+            _ => false
+        }
+        
+    }
+
     fn check(&self, lookup : usize , dir: Dir) -> (Option<usize>, Option<usize>){
         let mut res = (None, None);
         match dir{
@@ -86,7 +100,7 @@ impl InnerState{
         if is_x{
             self.inner_state = [1;9]
         }else{
-            self.inner_state = [4;9]
+            self.inner_state = [3;9]
         }
     }
 }
@@ -111,25 +125,61 @@ impl State{
             prev : vec![InnerState::default(); size_hint.unwrap_or(0)],
             curr : vec![InnerState::default(); size_hint.unwrap_or(0)],
             mapper : HashMap::from([
-                ('X', 1),
-                ('M', 2),
-                ('A', 3),
-                ('S', 4),
+                //('X', 1),
+                ('M', 1),
+                ('A', 2),
+                ('S', 3),
             ])
         }
     }
 
     fn process(&mut self, index: usize, ch : char) -> u32{
+        // // up
+        // res += self.compare_and_update(Dir::Ver,ch, index);
+        // // right up
+        // res += self.compare_and_update(Dir::LeftD,ch, index);
+        // // left up
+        // res += self.compare_and_update(Dir::RightD,ch, index);
+        // // left
+        // res += self.compare_and_update(Dir::Hor,ch, index);
         let mut res = 0;
-        
-        // up
-        res += self.compare_and_update(Dir::Ver,ch, index);
-        // right up
-        res += self.compare_and_update(Dir::LeftD,ch, index);
-        // left up
-        res += self.compare_and_update(Dir::RightD,ch, index);
-        // left
-        res += self.compare_and_update(Dir::Hor,ch, index);
+        match ch{
+            'A' => {
+                if index > 0 {
+                    let inner = &self.prev[(index-1)];
+                    if inner[4] == 1 || inner[4] ==3{
+                        self.curr[index][0] = inner[4]
+                    }
+                }
+                if let Some(inner) = self.prev.get(index+1){
+                    if inner[4] == 1 || inner[4] ==3{
+                        self.curr[index][2] = inner[4]
+                    }
+                }
+                self.curr[index][4] = 2;
+            },
+            'M' | 'S' => {
+                //check upper leftd
+                if index > 0{
+                    let inner = &self.prev[(index-1)];
+                    if inner[4] == 2{
+                        self.prev[index-1][8] = self.mapper[&ch] as u32;
+                    }
+                    if self.prev[index-1].check_diag(){
+                        //println!("what is index {}", index -1);
+                        res+= 1;
+                    }
+                }
+
+                //check upper rightd
+                if let Some(inner) = self.prev.get(index+1){
+                    if inner[4] == 2{
+                        self.prev[index+1][6] = self.mapper[&ch] as u32;
+                    }
+                }
+            },
+            _ => ()
+        }
 
         self.update_init(ch,index);
         
@@ -138,7 +188,7 @@ impl State{
 
     fn update_init(&mut self, ch: char, index : usize){
         match ch{
-            'X' => self.curr[index].populate_for_x_s(true),
+            'M' => self.curr[index].populate_for_x_s(true),
             'S' => self.curr[index].populate_for_x_s(false),
             _ => ()
         }
@@ -185,12 +235,12 @@ impl State{
 
 fn process(mut buffer : impl Iterator<Item = Result<String, std::io::Error>>) -> Option<u32>{
     let mut scan_str = buffer.next()?.ok()?;
-    println!("what is input {}", scan_str);
+    //println!("what is input {}", scan_str);
     let mut state = State::init(Some(scan_str.len()));
     let mut res = 0;
     
     loop{
-        println!("what is prev state: {:?}" ,state.prev);
+        //println!("what is prev state: {:?}" ,state.prev);
         for (index, ch) in scan_str.char_indices(){
            // println!("ch {}" , ch == 'X');
             res += state.process(index, ch);
@@ -215,10 +265,35 @@ struct State{
 mod tests{
     use super::*;
 
+    // #[test]
+    // fn test1(){
+    //     let vec : Vec<Result<String,std::io::Error>> = vec![Ok("XMAXMASAMXAS".to_string()), Ok("MAXSXAMAXAMA".to_string())];
+    //     let  res = process(vec.into_iter());
+    //     assert_eq!(None, res);
+    // }
+
     #[test]
-    fn test1(){
-        let vec : Vec<Result<String,std::io::Error>> = vec![Ok("XMAXMASAMXAS".to_string()), Ok("MAXSXAMAXAMA".to_string())];
-        let  res = process(vec.into_iter());
-        assert_eq!(None, res);
+    fn test2(){
+        //let vec : Vec<Result<String,std::io::Error>> = vec![Ok("XMAXMASAMXAS".to_string()), Ok("MAXSXAMAXAMA".to_string()), Ok("MAXSXAMAXAMA".to_string())];
+        let nvec = vec![".M.S......","..A..MSMS.",".M.S.MAA..","..A.ASMSM.",".M.S.M....","..........","S.S.S.S.S.",".A.A.A.A..","M.M.M.M.M.",".........."];
+        let vec : Vec<Result<String,std::io::Error>> = nvec.into_iter().map(|e| Ok(e.to_string())).collect();
+        let res = process(vec.into_iter());
+        assert_eq!(Some(9), res);
+    }
+
+    #[test]
+    fn test3(){
+        let nvec = vec!["MMMM", "AAAA", "SSSS", "AAAA", "MMMM", "AXXA", "SSSS", "AAAA", "MMMM"];
+        let vec : Vec<Result<String,std::io::Error>> = nvec.into_iter().map(|e| Ok(e.to_string())).collect();
+        let res = process(vec.into_iter());
+        assert_eq!(Some(6), res);
+    }
+
+    #[test]
+    fn test4(){
+        let nvec = vec!["MMMMASSSS"];
+        let vec : Vec<Result<String,std::io::Error>> = nvec.into_iter().map(|e| Ok(e.to_string())).collect();
+        let res = process(vec.into_iter());
+        assert_eq!(Some(0), res);
     }
 }
